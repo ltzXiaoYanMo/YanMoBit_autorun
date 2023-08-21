a84fde70aa46aab026ed6b138e733b13b7fbc476
ltzXiaoYanMo <115706851+ltzXiaoYanMo@users.noreply.github.com>
2023-08-21 20:45:02 +0800

    Update autorun_centos.sh

--- autorun_centos.sh
+++ autorun_centos.sh
@@ -1,1 +1,1 @@
-sudo yum update && sudo yum install openjdk-17-jdk wget git python3 redis mysql-server -y && mkdir Bot && cd Bot && git clone https://github.com/ltzXiaoYanMo/YanBot_KHB_Edition && cd YanBot_KHB_Edition && python3 get-pip.py && pip3 install pdm && pdm install && pdm run python3 main.py
+sudo yum update && sudo yum install openjdk-17-jdk wget git python3 redis mysql-server -y && mkdir Bot && cd Bot && git clone https://github.com/ltzXiaoYanMo/YanBot_KHB_Edition.git && cd YanBot_KHB_Edition && python3 get-pip.py && pip3 install pdm && pdm install && pdm run python3 main.py
