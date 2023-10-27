32fd4580f550ff3d5a09c48687c5a4987cedfc27
ltzXiaoYanMo <xiajiaruimail@qq.com>
2023-10-27 19:51:22 +0800

    修改RockyLinux的安装，并进行美化

--- autorun_rocky_mirai.sh
+++ autorun_rocky_mirai.sh
@@ -1,1 +1,11 @@
-sudo dnf update && sudo dnf install -y wget zip unzip java-17-openjdk-devel && wget https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.2/mcl-2.1.2.zip && cd ~ && mkdir MCL && mv mcl-2.1.2.zip MCL && cd MCL && unzip mcl-2.1.2.zip && sudo chmod +x * && ./mcl -u
+# 虽然改这玩意没啥好改的，但是为了你们更好提Issue和PR，我还是美化下罢（
+
+# 更新
+sudo dnf update
+# 安装
+sudo dnf install -y wget zip unzip java-17-openjdk-devel
+# 安装（Mirai）
+wget https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.2/mcl-2.1.2.zip && cd ~ && mkdir MCL && mv mcl-2.1.2.zip MCL && cd MCL
+unzip mcl-2.1.2.zip && sudo chmod +x *
+# Mirai启动
+./mcl -u
